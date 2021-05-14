0596aeba8e451546d17e3c03216c6b20b48fc344
eflipe <felipecabaleiro@gmail.com>
2021-05-14 13:47:02 -0300

    02 - Images model

--- app_img/admin.py
+++ app_img/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Images
 
-# Register your models here.
+
+@admin.register(Images)
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created']
+    list_filter = ['created']
--- app_img/models.py
+++ app_img/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Images(models.Model):
+    title = models.CharField(max_length=200, blank=True)
+    url = models.URLField()
+    created = models.DateField(auto_now_add=True,
+                               db_index=True)
+
+
+    def __str__(self):
+        return self.title
